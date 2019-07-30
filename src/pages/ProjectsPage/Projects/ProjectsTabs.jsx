d6a4848307b12571011bba5f6d8ac07488e9531f
Alvaro <alvaro.serrano@wmich.edu>
2019-07-30 23:10:41 +0200

    Box needs to be fixed in ProjectsTabs

--- src/pages/ProjectsPage/Projects/ProjectsTabs.jsx
+++ src/pages/ProjectsPage/Projects/ProjectsTabs.jsx
@@ -1,10 +1,15 @@
 import React from "react"
-import CardContent from "./CardContent"
-import CustomTabs from "../../../components/CustomTabs/CustomTabs"
-import tabsStyle from "../../../assets/jss/material-kit-react/views/componentsSections/tabsStyle.jsx"
+import PropTypes from "prop-types"
 import AppBar from "@material-ui/core/AppBar"
 import Tabs from "@material-ui/core/Tabs"
 import Tab from "@material-ui/core/Tab"
+import PhoneIcon from "@material-ui/icons/Phone"
+import FavoriteIcon from "@material-ui/icons/Favorite"
+import PersonPinIcon from "@material-ui/icons/PersonPin"
+import HelpIcon from "@material-ui/icons/Help"
+import ShoppingBasket from "@material-ui/icons/ShoppingBasket"
+import ThumbDown from "@material-ui/icons/ThumbDown"
+import ThumbUp from "@material-ui/icons/ThumbUp"
 import Typography from "@material-ui/core/Typography"
 
 function TabPanel(props) {
@@ -15,21 +20,27 @@ function TabPanel(props) {
       component="div"
       role="tabpanel"
       hidden={value !== index}
-      id={`scrollable-auto-tabpanel-${index}`}
-      aria-labelledby={`scrollable-auto-tab-${index}`}
+      id={`scrollable-force-tabpanel-${index}`}
+      aria-labelledby={`scrollable-force-tab-${index}`}
       {...other}
     />
   )
 }
 
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.any.isRequired,
+  value: PropTypes.any.isRequired,
+}
+
 function a11yProps(index) {
   return {
-    id: `scrollable-auto-tab-${index}`,
-    "aria-controls": `scrollable-auto-tabpanel-${index}`,
+    id: `scrollable-force-tab-${index}`,
+    "aria-controls": `scrollable-force-tabpanel-${index}`,
   }
 }
 
-export default function ScrollableTabsButtonAuto() {
+export default function ScrollableTabsButtonForce() {
   const [value, setValue] = React.useState(0)
 
   function handleChange(event, newValue) {
@@ -42,23 +53,23 @@ export default function ScrollableTabsButtonAuto() {
         <Tabs
           value={value}
           onChange={handleChange}
+          variant="scrollable"
+          scrollButtons="on"
           indicatorColor="primary"
           textColor="primary"
-          variant="scrollable"
-          scrollButtons="auto"
-          aria-label="scrollable auto tabs example"
+          aria-label="scrollable force tabs example"
         >
-          <Tab label="Item One" {...a11yProps(0)} />
-          <Tab label="Item Two" {...a11yProps(1)} />
-          <Tab label="Item Three" {...a11yProps(2)} />
-          <Tab label="Item Four" {...a11yProps(3)} />
-          <Tab label="Item Five" {...a11yProps(4)} />
-          <Tab label="Item Six" {...a11yProps(5)} />
-          <Tab label="Item Seven" {...a11yProps(6)} />
+          <Tab label="Item One" icon={<PhoneIcon />} {...a11yProps(0)} />
+          <Tab label="Item Two" icon={<FavoriteIcon />} {...a11yProps(1)} />
+          <Tab label="Item Three" icon={<PersonPinIcon />} {...a11yProps(2)} />
+          <Tab label="Item Four" icon={<HelpIcon />} {...a11yProps(3)} />
+          <Tab label="Item Five" icon={<ShoppingBasket />} {...a11yProps(4)} />
+          <Tab label="Item Six" icon={<ThumbDown />} {...a11yProps(5)} />
+          <Tab label="Item Seven" icon={<ThumbUp />} {...a11yProps(6)} />
         </Tabs>
       </AppBar>
       <TabPanel value={value} index={0}>
-        <CardContent />
+        itmedo
       </TabPanel>
       <TabPanel value={value} index={1}>
         Item Two
