c5a9ee3f8fa2cd0dcf4e25e56fa21ff33571572f
Alvaro <alvaro.serrano@wmich.edu>
2019-08-01 20:23:27 +0200

    changed background image in landing page

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -44,7 +44,7 @@ class LandingPage extends React.Component {
           }}
           {...rest}
         />
-        <Parallax filter image={require("assets/img/landing-bg.jpg")}>
+        <Parallax filter image={require("assets/img/landing2-bg.jpeg")}>
           <div className={classes.container}>
             <GridContainer>
               <GridItem xs={12} sm={12} md={6}>
