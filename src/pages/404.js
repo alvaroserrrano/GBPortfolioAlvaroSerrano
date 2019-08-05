4b4abe84134aa20bb996f0fae2a0cbc9c1006ea1
Alvaro <alvaro.serrano@wmich.edu>
2019-08-05 18:45:07 +0200

    finished custom 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,13 +4,76 @@ import notFoundPageStyle from "assets/jss/material-kit-react/views/notFoundPageS
 // @material-ui/core components
 import withStyles from "@material-ui/core/styles/withStyles"
 import Header from "components/Header/Header.jsx"
+import HeaderLinks from "components/Header/HeaderLinks.jsx"
 import Footer from "components/Footer/Footer.jsx"
+import Parallax from "components/Parallax/Parallax.jsx"
+import Button from "components/CustomButtons/Button.jsx"
+import GridContainer from "components/Grid/GridContainer.jsx"
+import GridItem from "components/Grid/GridItem.jsx"
+
+// React icons
+import { FaLinkedin } from "react-icons/fa"
+
+//Sections
+import WorkSection from "./LandingPage/Sections/WorkSection.jsx"
+
+const dashboardRoutes = []
 
 class NotFound extends React.Component {
   render() {
+    const { classes, ...rest } = this.props
     return (
       <div>
-        <Header />
+        <Header
+          color="transparent"
+          routes={dashboardRoutes}
+          brand="Alvaro Serrano"
+          rightLinks={<HeaderLinks />}
+          fixed
+          changeColorOnScroll={{
+            height: 400,
+            color: "white",
+          }}
+          {...rest}
+        />
+        <Parallax filter image={require("assets/img/landing2-bg.jpeg")}>
+          <div className={classes.container}>
+            <GridContainer>
+              <GridItem xs={12} sm={12} md={6}>
+                <h1 className={classes.title}>404 error</h1>
+                {/* #57BFC7 */}
+                {/* #D1366D */}
+                <h4 style={{ color: "#00b200" }}>
+                  <strong>
+                    The requested path was not found on this server
+                  </strong>
+                </h4>
+                <br />
+                <Button
+                  color="facebook"
+                  size="lg"
+                  href="https://www.linkedin.com/in/alvaro-serrano-rivas/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <FaLinkedin />
+                  Stay tuned
+                </Button>
+              </GridItem>
+            </GridContainer>
+          </div>
+        </Parallax>
+        <div className={classNames(classes.main, classes.mainRaised)}>
+          <div className={classes.container}>
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <WorkSection />
+          </div>
+        </div>
+        <Footer />
       </div>
     )
   }
