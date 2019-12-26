d3c329c756a2c139925e7b9b68bc31439e77942e
Alvaro <alvaro.serrano@wmich.edu>
2019-12-26 18:12:30 +0100

    email form submission input

--- src/pages/LandingPage/Sections/WorkSection.jsx
+++ src/pages/LandingPage/Sections/WorkSection.jsx
@@ -92,7 +92,9 @@ class WorkSection extends React.Component {
                     md={4}
                     className={classes.textCenter}
                   >
-                    <Button color="primary">Send Message</Button>
+                    <Button type="submit" color="primary">
+                      Send Message
+                    </Button>
                   </GridItem>
                 </GridContainer>
               </GridContainer>
